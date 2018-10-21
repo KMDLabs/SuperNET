86de24a6595c44752a69363dccb30442f0d41cb4
blackjok3r <blackjok3r@gmail.com>
2018-10-22 03:40:12 +0800

    a

--- crypto777/iguana_utils.c
+++ crypto777/iguana_utils.c
@@ -382,7 +382,7 @@ unsigned char _decode_hex(char *hex) { return((unhex(hex[0])<<4) | unhex(hex[1])
 int32_t decode_hex(unsigned char *bytes,int32_t n,char *hex)
 {
     int32_t adjust,i = 0;
-  	printf("decode.(%s) len.%d\n",hex,n);
+  	printf("decode.(%s) len.%d\n",hex,n*2-1);
     if ( isahexstr(hex,n*2-1) <= 0 )
     {
         memset(bytes,0,n);
