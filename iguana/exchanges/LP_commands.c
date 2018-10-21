6c0ac08bee2d9c63bba61716649d517b9e8dafb1
blackjok3r <blackjok3r@gmail.com>
2018-10-21 20:37:54 +0800

    ?

--- iguana/exchanges/LP_commands.c
+++ iguana/exchanges/LP_commands.c
@@ -747,7 +747,7 @@ version\n\
             {
                 if ( (ptr= LP_coinsearch(coin)) != 0 ) {
                     printf("starting tx blaster thread.\n");
-                    return(jprint(LP_txblast(ptr,argjson)));
+                    return(jprint(LP_txblast(ptr,argjson),1));
                     /*txblast_args* args = malloc(sizeof *args);
                     if (args != NULL) {
                         args->coin = ptr;
