564cc5cda0ef54b2f471b79975f8eaa35ae9985e
blackjok3r <blackjok3r@gmail.com>
2018-10-21 10:49:26 +0800

    try

--- iguana/exchanges/LP_transaction.c
+++ iguana/exchanges/LP_transaction.c
@@ -1753,9 +1753,10 @@ char *LP_streamerqadd(cJSON *argjson) {
         return(clonestr("{\"error\":\"hex string is invaild size.\"}"));
     chunk->datalen = chunklen / 2;
     fprintf(stderr, "about to decode hex: %s len.(%d)\n",data,chunklen);
-    if ( is_hexstr(data,strlen(data)-1) != 1 )
+    //if ( is_hexstr(data,strlen(data)-1) != 1 )
+    //    return(clonestr("{\"error\":\"invalid hex string.\"}"));
+    if ( decode_hex(chunk->data,chunk->datalen,data) == 0)
         return(clonestr("{\"error\":\"invalid hex string.\"}"));
-    decode_hex(chunk->data,chunk->datalen,data);
     if ( init_lock == 0 )
     {
         portable_mutex_init(&streamerlock);
