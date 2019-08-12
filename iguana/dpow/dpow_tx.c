710365d96b7f0b26a33fe545c0a5c1bf227c0fef
blackjok3r <blackjok3r@gmail.com>
2019-08-13 01:33:24 +0800

    .

--- iguana/dpow/dpow_tx.c
+++ iguana/dpow/dpow_tx.c
@@ -153,15 +153,11 @@ uint64_t dpow_maskmin(uint64_t refmask, struct dpow_info *dp,struct dpow_block *
     }
     printf("\n");
     for (j=0; j<bp->numnotaries; j++)
-    {                    
-        //if ( (recvmask & (1LL << j)) == 0 ) 
-        //    continue;
+    {
         k = DPOW_MODIND(bp,j,dp->freq);
-        if ( (recvmask & (1LL << k)) == 0 ) 
+        if ( (bp->recvmask & (1LL << k)) == 0 ) 
             k += rndnodes[j>>];
-        if ( k >= 64 ) k -= 64;
-        if ( (recvmask & (1LL << k)) == 0 ) 
-            continue;
+        if ( k >= bp->numnotaries ) k -= bp->numnotaries;
         if ( bits256_nonz(bp->notaries[k].src.prev_hash) != 0 && bits256_nonz(bp->notaries[k].dest.prev_hash) != 0 && bp->paxwdcrc == bp->notaries[k].paxwdcrc )
         {
             mask |= (1LL << k);
