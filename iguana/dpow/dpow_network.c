38807b9f29ece60e8062ce0219cd9fb80776101b
blackjok3r <blackjok3r@gmail.com>
2019-10-24 14:08:30 +0800

    remove print

--- iguana/dpow/dpow_network.c
+++ iguana/dpow/dpow_network.c
@@ -1665,9 +1665,9 @@ void dpow_oldbestconsensus(struct dpow_info *dp,struct dpow_block *bp)
         bp->bestmatches = bestmatches;
         bp->notaries[bp->myind].bestmask = bp->bestmask = masks[besti];
         bp->notaries[bp->myind].bestk = bp->bestk = bestks[besti];
-        //if ( bp->myind == 0 )
+        if ( 0 && bp->myind == 0 )
             printf("matches.%d bestmatches.%d recv.%llx (%d %llx)\n",matches,bestmatches,(long long)bp->recvmask,bp->bestk,(long long)bp->bestmask);
-        //if ( 1 && bp->myind == 0 && strcmp("KMD",dp->symbol) == 0 )
+        if ( 0 && bp->myind == 0 && strcmp("KMD",dp->symbol) == 0 )
         {
             for (i=0; i<bp->numnotaries; i++)
                 printf("%d:%d%s ",wts[i],owts[i],wts[i]*owts[i]>median?"*":"");
